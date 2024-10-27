cf6ab926c01ab0102b1e379da36929b8eee83273
P.D.KAVIN NISHANTHAN <106678494+KavinNishanthan@users.noreply.github.com>
2024-10-27 22:24:59 +0530

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-// import authController from '../controllers/auth.controller';
+
 import authController from '../controllers/auth.controller';
 
 const router = Router();
 
-// Register route
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
