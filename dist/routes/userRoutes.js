9c98c5d1a75bbcc688405d51a7f8afa037b58f97
Malarselvi <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-10-27 22:22:24 +0530

    Commit-1

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
+// import authController from '../controllers/auth.controller';
+const auth_controller_1 = __importDefault(require("../controllers/auth.controller"));
 const router = (0, express_1.Router)();
 // Register route
-router.post('/register', authController_1.register);
+router.post('/register', auth_controller_1.default.register);
+router.post('/login', auth_controller_1.default.login);
 exports.default = router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,22 +1,11 @@
 import { Router } from 'express';
-import authController from '../controllers/authController';
-// import loginController from '../controllers/loginController';
-import passport from 'passport';
+// import authController from '../controllers/auth.controller';
+import authController from '../controllers/auth.controller';
 
 const router = Router();
 
 // Register route
 router.post('/register', authController.register);
-
-// Login route
-// router.post('/login', loginController.login);
-
-// Google sign-in route
-router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-}));
+router.post('/login', authController.login);
 
 export default router;
